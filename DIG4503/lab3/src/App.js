7b4e6898af41398ea0d63c64625cfd1b7ce623eb
rcollins1106 <49650574+rcollins1106@users.noreply.github.com>
2021-02-06 22:01:03 -0500

    Add files via upload

---
+++ DIG4503/lab3/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+
+class HomePage extends React.Component{
+    constructor(firstName){
+        super(Randall);
+    }
+    
+    
+    render(){
+
+        return(<p{this.firstName.attr}/)
+      }
+}
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
+export default HomePage;
