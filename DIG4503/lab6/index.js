42c72ec6583b0bb9a931e55b08a5aac1c41bba5e
rcollins1106 <49650574+rcollins1106@users.noreply.github.com>
2021-02-27 21:06:32 -0500

    Add files via upload

--- DIG4503/lab6/index.js
+++ DIG4503/lab6/index.js
@@ -1,1 +1,23 @@
+import Express from 'express';
 
+const App = Express();
+const port = 45030;
+
+
+const person = {
+    color: "Green",
+    name: "Randall"
+};
+
+App.get("/", function(req, res){
+    res.send("Hello World!");
+
+});
+
+App.get("/person", (req, res) => {
+    res.json(person);
+});
+
+App.listen(port, function(){
+    console.log("Server running!");
+});
