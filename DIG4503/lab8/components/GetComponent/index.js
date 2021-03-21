d65f35b3603cc984ecceacee47a881e1a2d60e79
rcollins1106 <49650574+rcollins1106@users.noreply.github.com>
2021-03-21 07:21:31 -0400

    Update index.js

--- DIG4503/lab8/components/GetComponent/index.js
+++ DIG4503/lab8/components/GetComponent/index.js
@@ -2,20 +2,16 @@ import Axios from 'axios';
 import names from '../../server/index';
 
 Axios.get("/search/:name", (req, res) => {
-    // Save the request parameter.
-    let person = req.params.person;
 
-    // Setup a default response.
+    let person = req.params.person;
     let result = {person: "not found"};
 
-    // Search through the internal array.
     names.forEach(value => {
         if(value == person) {
             result = {person: names};
         }
     });
 
-    // Return the default or searched-for result.
     res.json(result);
 });
 
