eb5ffba2a471f6b4afcb549fadefd93510c02514
rcollins1106 <49650574+rcollins1106@users.noreply.github.com>
2021-03-21 07:19:04 -0400

    Update index.js

--- DIG4503/lab8/server/index.js
+++ DIG4503/lab8/server/index.js
@@ -23,62 +23,49 @@ const names = [
 
 // Create
 App.put("/people/:person", (req, res) => {
-    // Save the request parameter.
+    
     let person = req.params.person;
-    // Add to the internal array.
+    
     names.push(names)
-    // Return the name just created.
+    
     res.json({person: names});
 });
 
 // Read
 App.get("/people/:person", (req, res) => {
-    // Save the request parameter.
+    
     let person = req.params.person;
-
-    // Setup a default response.
     let result = {person: "not found"};
 
-    // Search through the internal array.
     names.forEach(value => {
         if(value == person) {
             result = {person: person};
         }
     });
-
-    // Return the default or searched-for result.
     res.json(result);
 });
 
 // Read
 App.get("/search/:name", (req, res) => {
-    // Save the request parameter.
-    let person = req.params.person;
 
-    // Setup a default response.
+    let person = req.params.person;
     let result = {person: "not found"};
 
-    // Search through the internal array.
     names.forEach(value => {
         if(value == person) {
             result = {person: person};
         }
     });
 
-    // Return the default or searched-for result.
     res.json(result);
 });
 
-// Update
-// Based on this example, there is nothing to update!
-
 // Delete
 App.delete("/people/:person", (req, res) => {
-    // Save the request parameter.
+
     let person = req.params.person;
-    // Delete an entry by creating an array without it.
     names = names.filter(value => value != person);
-    // Return the name just deleted (regardless if it worked or not).
+
     res.json({person: person});
 });
 
