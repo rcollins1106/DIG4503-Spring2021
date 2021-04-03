6db534c211703056a3cd817c975d1637c86d0063
rcollins1106 <49650574+rcollins1106@users.noreply.github.com>
2021-04-03 18:18:08 -0400

    Update index.js

--- DIG4503/lab10/index.js
+++ DIG4503/lab10/index.js
@@ -17,13 +17,14 @@ App.put("/people/create", (req, res) => {
     let result = d.createOne(person);
     // Return the name just created.
     res.json(result);
+    
 });
 
 App.get("/people/:person", (req, res) => {
     // Save the request parameter.
     let person = req.params.person;
     // Search the database.
-    let result = d.readMany(person);
+    let result = d.readOne(person);
     // Return the default or searched-for result.
     res.json(result);
 });
