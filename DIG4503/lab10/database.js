735324b67d1bbb8262d6472251b5ab5fe349fadc
rcollins1106 <49650574+rcollins1106@users.noreply.github.com>
2021-04-03 21:04:04 -0400

    Update database.js

--- DIG4503/lab10/database.js
+++ DIG4503/lab10/database.js
@@ -1,41 +1,23 @@
 import MongoClient from 'mongodb';
 
-
-
 const url = "mongodb+srv://RandallCollins:Lopvx0SD8NNuYaZA@cluster0.yuzwq.mongodb.net";
 MongoClient.connect(url, function(err, connection) {
 class Database {
-    constructor() {
-        this.arrayExample = [
-            'Cortney',
-            'Dewayne',
-            'Trenton',
-            'Pamala',
-            'Ettie',
-            'Errol',
-            'Lorrie',
-            'Hang',
-            'Lauryn',
-            'Caterina',
-            'Isa',
-            'Marcela'
-        ];
-    }
 
     async connect(){
         //Wait for the connect() method to finish.
         this.connection = await MongoClient.connect(url, {useUnifiedTopology: true});
         //Select a database.
-        this.database = this.connection.db("<lab10>");
+        this.database = this.connection.db("lab10");
         //Select a collection.
-        this.collection = this.database.collection("<people>");
+        this.collection = this.database.collection("people");
     }
 
     createOne(person) {
-        // Add to the internal array.
-        this.arrayExample.push(person);
+        // Add
+        this.collection.push(person);
         
-        //RANDALL
+        //insert
         collection.insertOne({
             "firstName": "Randall", 
             "lastName": "Collins",
@@ -43,7 +25,7 @@ class Database {
 
         });
 
-        // Return the name just created.
+        // Return
         return {person: person};
         
     }
@@ -53,13 +35,10 @@ class Database {
         let result = {person: "not found"};
 
         //Find
-        collection.findOne({"firstName": {$eq:""}}, {"lastName": {$eq:""}}, {"favoriteColor": {$eq:""}}
+        collection.findOne({"firstName": {$eq:"Randall"}}, {"lastName": {$eq:"Collins"}}, {"favoriteColor": {$eq:"Green"}}
         .then((results) => {
             console.log(result);
             connection.close();
-        })
-        .catch(error => {
-            console.log("Error: " + error);
         })
         );
 
